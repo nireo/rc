d2130e0e56aa3de09247c477ee452b489b426244
nireo <eemil.lehtikangas1@gmail.com>
2023-10-20 01:41:29 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod sexpr;
+use sexpr::*;
+
+fn main() {
+    println!("{:?}", SEXP_STRUCT.buffer_encode());
+    let ctx = &mut ParseContext::new(SEXP_STRING_IN);
+    println!("{:?}", SExp::parse(ctx));
+}
