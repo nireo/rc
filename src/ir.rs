3a758d33b55189bd6f3ca2c458a5fd0f7d64fafc
nireo <eemil.lehtikangas1@gmail.com>
2023-10-21 02:24:44 +0300

    started working on ir generation

--- src/ir.rs
+++ src/ir.rs
@@ -1,6 +1,26 @@
+use crate::sexpr::*;
+use anyhow::anyhow;
+use anyhow::Result;
 use std::collections::HashMap;
 
-#[derive(Copy, Clone)]
+#[derive(Debug)]
+enum IrErrors {
+  DuplicateName,
+  UnknownExpression,
+  BadBinopTypes,
+}
+
+impl std::fmt::Display for IrErrors {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Self::DuplicateName => write!(f, "Duplicate variable name"),
+      Self::UnknownExpression => write!(f, "unknown expression"),
+      Self::BadBinopTypes => write!(f, "Unsuitable binop types"),
+    }
+  }
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
 enum Type {
   Int,
   Byte,
@@ -9,20 +29,46 @@ enum Type {
   Void,
 }
 
+#[derive(PartialEq, Debug)]
+enum Binop {
+  Plus,
+  Minus,
+  Multiply,
+  Divide,
+}
+
+#[derive(PartialEq, Debug)]
+enum Unop {
+  Minus,
+  Not,
+}
+
+#[derive(PartialEq, Debug)]
+enum Instruction {
+  Constant(i64, i64),
+  Label(i64),
+  Binop(Binop, i64, i64, i64, bool),
+  Mov(i64, i64),
+}
+
+type Types = Vec<Type>;
+type TypeIndex = (Types, i64);
+
 struct Scope<'a> {
   prev: Option<Box<Scope<'a>>>,
-  nlocals: usize,
-  names: HashMap<&'a str, (Type, usize)>,
+  nlocals: i64,
+  names: HashMap<&'a str, TypeIndex>,
 }
 
 struct Func<'a> {
   scope: Scope<'a>,
-  nvar: usize,
-  stack: usize,
+  nvar: i64,
+  stack: i64,
+  instructions: Vec<Instruction>,
 }
 
 impl<'a> Scope<'a> {
-  fn get_var(&self, name: &str) -> Option<(Type, usize)> {
+  fn get_var(&self, name: &str) -> Option<TypeIndex> {
     match self.names.get(name) {
       Some(val) => Some(val.to_owned()),
       None => {
@@ -34,6 +80,89 @@ impl<'a> Scope<'a> {
       }
     }
   }
+}
 
+impl<'a> Func<'a> {
+  fn tmp(&mut self) -> i64 {
+    let dst = self.stack;
+    self.stack += 1;
+    dst
+  }
 
+  // returns the address where the variable resides.
+  fn add_var(&mut self, name: &'a str, ty: Types) -> Result<i64> {
+    if self.scope.names.contains_key(name) {
+      Err(anyhow!(IrErrors::DuplicateName))
+    } else {
+      self.scope.names.insert(name, (ty, self.nvar));
+      self.scope.nlocals += 1;
+      assert!((self.stack == self.nvar), "stack is not equal to nvar");
+      let dst = self.stack;
+      self.stack += 1;
+      self.nvar += 1;
+      Ok(dst)
+    }
+  }
+
+  fn comp_expr(&mut self, sexpr: &SExp<'a>, allow_var: bool) -> Result<TypeIndex> {
+    if allow_var {
+      assert!((self.stack == self.nvar), "stack != nvar when allow_var");
+    }
+
+    let save = self.stack;
+    let type_index = self.comp_expr_tmp(sexpr, allow_var)?;
+    let var = type_index.1;
+    assert!(
+      (var < self.stack),
+      "returned addr outside of stack."
+    );
+
+    // Discard temporaries from the compilation above. Either the stack is local variables only
+    // or we revert it back to it's original state.
+    self.stack = if allow_var { self.nvar } else { save };
+
+    // The result is either a temporary stored at the top of the stack or a local variable.
+    assert!((var < self.stack), "returned addr outside of stack.");
+    Ok(type_index)
+  }
+
+  fn comp_expr_tmp(&mut self, sexpr: &SExp<'a>, allow_var: bool) -> Result<TypeIndex> {
+    match sexpr {
+      SExp::List(list) => {
+        let arg = list[0].as_str()?;
+        if arg == "+" && list.len() == 3 {
+          self.comp_binop(sexpr)
+        } else {
+          Err(anyhow!(IrErrors::UnknownExpression))
+        }
+      }
+      SExp::F64(num) => {
+        let dst = self.tmp();
+        self.instructions.push(Instruction::Constant(*num as i64, dst));
+        Ok((vec![Type::Int, Type::Byte, Type::BytePtr], dst))
+      }
+      SExp::Str(val) => todo!()
+    }
+  }
+
+  fn comp_binop(&mut self, sexpr: &SExp<'a>) -> Result<TypeIndex> {
+    // As with most compilation functions this is already ensured to work.
+    let list = sexpr.as_list()?;
+    let save = self.stack;
+    // TODO: convert binop from string and handle boolean comparisons.
+
+    // First element is the argument so that 1 and 2 are lhs and rhs.
+    let lhs = self.comp_expr_tmp(&list[1], false)?;
+    let rhs = self.comp_expr_tmp(&list[2], false)?;
+
+    self.stack = save;
+    if !(lhs.0 == rhs.0 && (lhs.0[0] == Type::Int || lhs.0[0] == Type::Byte)) {
+      Err(anyhow!(IrErrors::BadBinopTypes))
+    } else {
+      let dst = self.tmp();
+      let byte_exp = lhs.0 == rhs.0 && lhs.0 == vec![Type::Byte];
+      self.instructions.push(Instruction::Binop(Binop::Plus, lhs.1, rhs.1, dst, byte_exp));
+      Ok((lhs.0, dst))
+    }
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-mod sexpr;
 mod ir;
+mod sexpr;
 use sexpr::*;
 
-fn main() {
-    println!("{:?}", SEXP_STRUCT.buffer_encode());
-    let ctx = &mut ParseContext::new(SEXP_STRING_IN);
-    println!("{:?}", SExp::parse(ctx));
+fn main()  {
+  println!("{:?}", SEXP_STRUCT.buffer_encode());
+  let ctx = &mut ParseContext::new("(+ 1 2)");
+  let parsed = SExp::parse(ctx).unwrap();
 }
